90ab4c1ab446060a10b49abd16224527b12bd38f
reinist <reinist@gmail.com>
2022-04-04 19:47:53 +0300

    Add project files.

---
+++ Api2Csv/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+
+namespace ApiToCsv
+{
+    class Logger
+    {
+        //public StreamWriter w = File.AppendText(ConfigurationManager.AppSettings.Get("LogPath"));
+        public static void Append(string logMessage)
+        {
+
+            //File.AppendText(ConfigurationManager.AppSettings.Get("LogPath")).WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()} :{logMessage}");
+            //ile.AppendText(ConfigurationManager.AppSettings.Get("LogPath")).WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()} :{logMessage}");
+
+        }
+
+    }
+}
---
+++ Api2Csv/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.IO;
+
+
+namespace ApiToCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args[0].Split(','))
+            {
+                Console.WriteLine(arg);
+                Logger.Append(arg);
+            }
+
+            string sAttr;
+            sAttr = ConfigurationManager.AppSettings.Get("LogPath");
+            Console.WriteLine("The value of Key0 is " + sAttr);
+
+            // Read all the keys from the config file
+            NameValueCollection sAll;
+            sAll = ConfigurationManager.AppSettings;
+
+            foreach (string s in sAll.AllKeys)
+                Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+            Console.ReadLine();
+        }
+    }
+}
