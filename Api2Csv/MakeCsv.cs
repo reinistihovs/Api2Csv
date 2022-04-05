6cc0f0686883cca7e1dfac4d173680bb29dc18c6
reinist <reinist@gmail.com>
2022-04-05 11:05:08 +0300

    fixed error caching

--- Api2Csv/MakeCsv.cs
+++ Api2Csv/MakeCsv.cs
@@ -14,11 +14,10 @@ namespace Api2Csv
         public static void generate(string jsonContent)
         {
 
-
+            string csvPath = ConfigurationManager.AppSettings["csvPath"];
             //used NewtonSoft NuGet, its free for commercial use: https://www.newtonsoft.com/json
             try
-            {
-                string csvPath = ConfigurationManager.AppSettings["csvPath"];
+            {               
                 XmlNode xml = JsonConvert.DeserializeXmlNode("{records:{record:" + jsonContent + "}}");
                 XmlDocument xmldoc = new XmlDocument();
                 xmldoc.LoadXml(xml.InnerXml);
@@ -40,34 +39,27 @@ namespace Api2Csv
                 var valueRows = dataTable.AsEnumerable()
                                    .Select(row => string.Join(",", row.ItemArray));
                 rows.AddRange(valueRows);
-                try
-                {
-                    File.AppendAllLines(csvPath, rows);
-                }
-                catch (DirectoryNotFoundException e)
-                {
-                    Logger.Append($"Directory {csvPath} not found, error: {e}");
-                    throw new Exception($"Directory {csvPath} not found", e);
-                }
-                catch (UnauthorizedAccessException e)
-                {
-                    Logger.Append($"Could not access the directory {csvPath}, permission denied, error: {e}");
-                    throw new Exception($"Could not access the directory {csvPath}, permission denied", e);
-                }
-                catch (IOException e)
-                {
-                    Logger.Append($"Error during csv file write, error: {e}");
-                    throw new Exception("Error during csv file write", e);
-                }
-                catch (Exception e)
-                {
-                    Logger.Append($"Error occurred, cant create csv file, error: {e}");
-                    throw new Exception("Error occurred, cant create csv file", e);
-                }
+                File.AppendAllLines(csvPath, rows);
+                
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Logger.Append($"Directory {csvPath} not found, error: {e}");
+                throw new Exception($"Directory {csvPath} not found", e);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Logger.Append($"Could not access the directory {csvPath}, permission denied, error: {e}");
+                throw new Exception($"Could not access the directory {csvPath}, permission denied", e);
+            }
+            catch (IOException e)
+            {
+                Logger.Append($"Error during csv file write, error: {e}");
+                throw new Exception("Error during csv file write", e);
             }
             catch (Exception e)
             {
-                Logger.Append($"Failed to append csv file, error: {e}");
+                Logger.Append($"Error occurred, cant create csv file, error: {e}");
             }
         }
     }
